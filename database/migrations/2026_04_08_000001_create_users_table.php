1ca09305d0f2e8ef8465b5d427359f6448da44ca
papanie33 <papanie33@gmail.com>
2026-04-08 08:18:45 +0000

    Create users migration file

---
+++ database/migrations/2026_04_08_000001_create_users_table.php
@@ -0,0 +1,17 @@
+class CreateUsersTable extends Migration {
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
